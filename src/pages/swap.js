a5158472880c8bca509a771979baa985b4116f25
nuwrldnf8r <nuwrldnf8r@gmail.com>
2021-08-31 11:04:31 +0200

    Changed swap copy

--- src/pages/swap.js
+++ src/pages/swap.js
@@ -10,7 +10,7 @@ const SwapPage = (props) => {
             marginTop: 30,
             textAlign: 'center'
     }}>
-        <div>If you wish to buy or sell ELYS you can swap ELYS for FTM at ZooDEX</div>
+        <div>If you wish to buy or sell ELYS you can swap FTM for ELYS at ZooDEX</div>
         <a href="https://dex.zoocoin.cash/orders/market?inputCurrency=FTM&outputCurrency=0xd89cc0d2A28a769eADeF50fFf74EBC07405DB9Fc"
         style={{
             textDecoration: 'none',
